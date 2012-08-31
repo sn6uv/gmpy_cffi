93d054b8135b187f808fb45f85bf5a2621d1d90b
Marc Christiansen <tolot@solar-empire.de>
2012-08-31 14:42:32 +0200

    Implement mpz class, with most arithmetic operations, but missing argument checking. Adapt pi_digits.

--- _gmpy.py
+++ _gmpy.py
@@ -9,8 +9,9 @@ ffi = cffi.FFI()
 ffi.cdef("""
     typedef struct { ...; } __mpz_struct;
     typedef __mpz_struct *mpz_t;
+    typedef unsigned long mp_bitcnt_t;
 
-//    void mpz_init (mpz_t x);
+    void mpz_init (mpz_t x);
     void mpz_init_set_ui (mpz_t rop, unsigned long int op);
     void mpz_init_set_si (mpz_t rop, signed long int op);
     void mpz_init_set_d (mpz_t rop, double op);
@@ -24,15 +25,25 @@ ffi.cdef("""
     void mpz_add (mpz_t rop, mpz_t op1, mpz_t op2);
     void mpz_add_ui (mpz_t rop, mpz_t op1, unsigned long int op2);
     void mpz_sub (mpz_t rop, mpz_t op1, mpz_t op2);
+    void mpz_sub_ui (mpz_t rop, mpz_t op1, unsigned long int op2);
+    void mpz_ui_sub (mpz_t rop, unsigned long int op1, mpz_t op2);
     void mpz_mul (mpz_t rop, mpz_t op1, mpz_t op2);
     void mpz_mul_ui (mpz_t rop, mpz_t op1, unsigned long int op2);
     void mpz_submul (mpz_t rop, mpz_t op1, mpz_t op2);
-//    void mpz_neg (mpz_t rop, mpz_t op);
+    void mpz_mul_2exp (mpz_t rop, mpz_t op1, mp_bitcnt_t op2);
+    void mpz_neg (mpz_t rop, mpz_t op);
 
+    void mpz_fdiv_q (mpz_t q, mpz_t n, mpz_t d);
+    void mpz_fdiv_q_ui (mpz_t q, mpz_t n, unsigned long int d);
+    void mpz_fdiv_r (mpz_t r, mpz_t n, mpz_t d);
+    void mpz_fdiv_r_ui (mpz_t r, mpz_t n, unsigned long int d);
     void mpz_fdiv_qr (mpz_t q, mpz_t r, mpz_t n, mpz_t d);
+    void mpz_fdiv_qr_ui (mpz_t q, mpz_t r, mpz_t n, unsigned long int d);
+    void mpz_fdiv_q_2exp (mpz_t q, mpz_t n, mp_bitcnt_t b);
 //    int mpz_divisible_ui_p (mpz_t n, unsigned long int d);
 
     int mpz_cmp (mpz_t op1, mpz_t op2);
+    int mpz_cmp_ui (mpz_t op1, unsigned long int op2);
 
 //    void mpz_bin_ui (mpz_t rop, mpz_t n, unsigned long int k);
 //    void mpz_bin_uiui (mpz_t rop, unsigned long int n, unsigned long int k);
@@ -41,6 +52,15 @@ ffi.cdef("""
 gmp = ffi.verify("#include <gmp.h>", libraries=['gmp', 'm'])
 
 # ____________________________________________________________
+
+MAX_UI = 2 * sys.maxint + 1
+
+def _new_mpz():
+    """Return an initialized c mpz."""
+
+    mpz = ffi.new("mpz_t")
+    gmp.mpz_init(mpz)
+    return mpz
 
 def _pylong_to_mpz(n, a):
     """
@@ -61,35 +81,149 @@ def _pylong_to_mpz(n, a):
     addr, count = tmp.buffer_info()
     gmp.mpz_import(a, count, -1, tmp.itemsize, 0, 0, ffi.cast('void *', addr))
 
-def mpz(n, base=None):
-    """
-    Create a mpz from `n`.
+class mpz(object):
+    _mpz_str = None
 
-    `n`: Value to convert. Can be an int, a float or a string; if float, it gets truncated.
-    `base`: Base in which to interpret the string `n`. Only allowed if `n` is a string. If not given, `base` defaults to 10.
-    """
-    a = ffi.new("mpz_t")
-    if isinstance(n, str):
-        if base is None:
-            base = 10
-        if base == 0 or 2 <= base <= 62:
-            if gmp.mpz_init_set_str(a, n, base) == -1:
-                gmp.mpz_clear(a)
-                raise ValueError("Can't create mpz from %s with base %s" % (n, base))
+    def __init__(self, n, base=None):
+        """
+        Create a mpz from `n`.
+
+        `n`: Value to convert. Can be an int, a float or a string; if float, it gets truncated.
+        `base`: Base in which to interpret the string `n`. Only allowed if `n` is a string. If not given, `base` defaults to 10.
+        """
+
+        a = self._mpz = ffi.new("mpz_t")
+        if isinstance(n, str):
+            if base is None:
+                base = 10
+            if base == 0 or 2 <= base <= 62:
+                if gmp.mpz_init_set_str(a, n, base) == -1:
+                    gmp.mpz_clear(a)
+                    raise ValueError("Can't create mpz from %s with base %s" % (n, base))
+            else:
+                raise ValueError('base must be 0 or 2..62, not %s' % base)
+        elif base is not None:
+            raise ValueError('Base only allowed for str, not for %s.' % type(n))
+        elif isinstance(n, float):
+            gmp.mpz_init_set_d(a, n)
+        elif -sys.maxint - 1 <= n <= sys.maxint:
+            gmp.mpz_init_set_si(a, n)
+        elif n <= MAX_UI:
+            gmp.mpz_init_set_ui(a, n)
         else:
-            raise ValueError('base must be 0 or 2..62, not %s' % base)
-    elif base is not None:
-        raise ValueError('Base only allowed for str, not for %s.' % type(n))
-    elif isinstance(n, float):
-        gmp.mpz_init_set_d(a, n)
-    elif -sys.maxint - 1 <= n <= sys.maxint:
-        gmp.mpz_init_set_si(a, n)
-    elif n <= 2 * sys.maxint + 1:
-        gmp.mpz_init_set_ui(a, n)
-    else:
-        neg = n < 0
-        _pylong_to_mpz(abs(n), a)
-        if neg:
-            gmp.mpz_neg(a, a)
+            neg = n < 0
+            _pylong_to_mpz(abs(n), a)
+            if neg:
+                gmp.mpz_neg(a, a)
 
-    return a
+    @classmethod
+    def _from_c_mpz(cls, mpz):
+        inst = object.__new__(cls)
+        inst._mpz = mpz
+        return inst
+
+    def __str__(self):
+        if self._mpz_str is None:
+            self._mpz_str = ffi.string(gmp.mpz_get_str(ffi.NULL, 10, self._mpz))
+        return self._mpz_str
+
+    def __repr__(self):
+        return 'mpz(%s)' % self
+
+    def __add__(self, other):
+        res = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_add_ui(res, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_add(res, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(res)
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        res = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_sub_ui(res, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_sub(res, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(res)
+
+    def __rsub__(self, other):
+        res = _new_mpz()
+        gmp.mpz_ui_sub(res, other, self._mpz)
+        return mpz._from_c_mpz(res)
+
+    def __mul__(self, other):
+        res = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_mul_ui(res, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_mul(res, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(res)
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        q = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_fdiv_q_ui(q, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_fdiv_q(q, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(q)
+
+    def __rdiv__(self, other):
+        q = _new_mpz()
+        gmp.mpz_fdiv_q(q, mpz(other)._mpz, self._mpz)
+        return mpz._from_c_mpz(q)
+
+    __floordiv__ = __div__
+    __rfloordiv__ = __rdiv__
+
+    def __mod__(self, other):
+        r = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_fdiv_r_ui(r, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_fdiv_r(r, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(r)
+
+    def __rmod__(self, other):
+        r = _new_mpz()
+        gmp.mpz_fdiv_r(r, mpz(other)._mpz, self._mpz)
+        return mpz._from_c_mpz(r)
+
+    def __divmod__(self, other):
+        q = _new_mpz()
+        r = _new_mpz()
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            gmp.mpz_fdiv_qr_ui(q, r, self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            gmp.mpz_fdiv_qr(q, r, self._mpz, oth._mpz)
+        return mpz._from_c_mpz(q), mpz._from_c_mpz(r)
+
+    def __rdivmod__(self, other):
+        q = _new_mpz()
+        r = _new_mpz()
+        gmp.mpz_fdiv_qr(q, r, mpz(other)._mpz, self._mpz)
+        return mpz._from_c_mpz(q), mpz._from_c_mpz(r)
+
+    def __lshift__(self, other):
+        res = _new_mpz()
+        gmp.mpz_mul_2exp(res, self._mpz, other)
+        return mpz._from_c_mpz(res)
+
+    def __rshift__(self, other):
+        res = _new_mpz()
+        gmp.mpz_fdiv_q_2exp(res, self._mpz, other)
+        return mpz._from_c_mpz(res)
+
+    def __cmp__(self, other):
+        if isinstance(other, int) and 0 <= other <= MAX_UI:
+            return gmp.mpz_cmp_ui(self._mpz, other)
+        else:
+            oth = mpz(other) if isinstance(other, int) else other
+            return gmp.mpz_cmp(self._mpz, oth._mpz)
--- pi_digits.py
+++ pi_digits.py
@@ -1,11 +1,5 @@
 import sys
-from _gmpy import *
-
-def inc(a, b): gmp.mpz_add_ui(a, a, b)
-def iadd(a, b): gmp.mpz_add(a, a, b)
-def imul(a, b): gmp.mpz_mul(a, a, b)
-def imul10(a): gmp.mpz_mul_ui(a, a, 10)
-def mpz_to_str(a): return ffi.string(gmp.mpz_get_str(ffi.NULL, 10, a))
+from _gmpy import mpz
 
 def main(N):
     i = 0
@@ -14,30 +8,27 @@ def main(N):
     n, a, d, t, u = mpz(1), mpz(0), mpz(1), mpz(0), mpz(0)
     tmp = mpz(0)
     while True:
-        inc(k, 1)
-        gmp.mpz_mul_ui(t, n, 2)
-        imul(n, k)
-        iadd(a, t)
-        inc(k1, 2)
-        imul(a, k1)
-        imul(d, k1)
-        if gmp.mpz_cmp(a, n) >= 0:
-            gmp.mpz_mul_ui(tmp, n, 3)
-            iadd(tmp, a)
-            gmp.mpz_fdiv_qr(t, u, tmp, d)
-            iadd(u, n)
-            if gmp.mpz_cmp(d, u) > 0:
-                imul10(ns)
-                iadd(ns, t)
+        k += 1
+        t = n << 1
+        n *= k
+        a += t
+        k1 += 2
+        a *= k1
+        d *= k1
+        if a >= n:
+            t, u = divmod(n * 3 + a, d)
+            u += n
+            if d > u:
+                ns = ns * 10 + t
                 i += 1
                 if i % 10 == 0:
-#                    print ('%s\t:%d' % (mpz_to_str(ns).zfill(10), i))
+                    print ('%s\t:%d' % (str(ns).zfill(10), i))
                     ns = mpz(0)
                 if i >= N:
                     break
-                gmp.mpz_submul(a, d, t)
-                imul10(a)
-                imul10(n)
+                a -= d * t
+                a *= 10
+                n *= 10
 
 if __name__ == "__main__":
     main(int(sys.argv[1]) if len(sys.argv) > 1 else 100)
