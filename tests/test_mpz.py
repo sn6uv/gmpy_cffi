3d4c32698ad3443166513bbb2580486696b8ed3d
Angus Griffith <16sn6uv@gmail.com>
2013-11-06 12:44:54 +1100

    replace sets with lists in tests for python2.6

--- tests/test_mpz.py
+++ tests/test_mpz.py
@@ -10,19 +10,19 @@ if sys.version > '3':
 
 
 class TestInit(object):
-    small_ints = {-1, 0, 1, 123, -9876, sys.maxsize, -sys.maxsize - 1}
-    big_ints = {sys.maxsize + 1, -sys.maxsize - 2, 2 * sys.maxsize + 1, 2 * sys.maxsize + 2}
+    small_ints = [-1, 0, 1, 123, -9876, sys.maxsize, -sys.maxsize - 1]
+    big_ints = [sys.maxsize + 1, -sys.maxsize - 2, 2 * sys.maxsize + 1, 2 * sys.maxsize + 2]
 
-    @pytest.mark.parametrize('n', small_ints.union(big_ints))
+    @pytest.mark.parametrize('n', small_ints + big_ints)
     def test_init_int(self, n):
         assert mpz(n) == n
 
-    @pytest.mark.parametrize('f', {0.0, 1.0, 1.5, 1e15 + 0.9})
+    @pytest.mark.parametrize('f', [0.0, 1.0, 1.5, 1e15 + 0.9])
     def test_init_float(self, f):
         assert mpz(f) == int(f)
         assert mpz(-f) == int(-f)
 
-    @pytest.mark.parametrize('n', small_ints.union(big_ints))
+    @pytest.mark.parametrize('n', small_ints + big_ints)
     def test_init_decimal_str(self, n):
         assert mpz(str(n), 10) == n
         assert mpz(str(n)) == n
@@ -30,28 +30,28 @@ class TestInit(object):
         assert mpz(hex(n).rstrip('L'), 0) == n
         assert mpz(oct(n).rstrip('L'), 0) == n
 
-    @pytest.mark.parametrize('n', small_ints.union(big_ints))
+    @pytest.mark.parametrize('n', small_ints + big_ints)
     def test_init_hex_str(self, n):
         assert mpz("%x" % n, 16) == n
         assert mpz("%#x" % n, 0) == n
 
-    @pytest.mark.parametrize(('n', 'base'), {('0x1', 16), ('g', 16), ('a', 10)})
+    @pytest.mark.parametrize(('n', 'base'), [('0x1', 16), ('g', 16), ('a', 10)])
     def test_init_invalid_str(self, n, base):
         with pytest.raises(ValueError):
             mpz(n, base)
 
-    @pytest.mark.parametrize(('n', 'base'), {('0', -1), ('0', 1), ('0', 63), (0, 10)})
+    @pytest.mark.parametrize(('n', 'base'), [('0', -1), ('0', 1), ('0', 63), (0, 10)])
     def test_init_invalid_base(self, n, base):
         with pytest.raises(ValueError):
             mpz(n, base)
 
-    @pytest.mark.parametrize('type_', {int, float, mpz, str})
+    @pytest.mark.parametrize('type_', [int, float, mpz, str])
     def test_init_type(self, type_):
         assert mpz(type_(1)) == 1
 
 
 class TestMath(object):
-    numbers = {-1, 0, 1, sys.maxsize, -sys.maxsize - 1, MAX_UI, MAX_UI + 1}
+    numbers = [-1, 0, 1, sys.maxsize, -sys.maxsize - 1, MAX_UI, MAX_UI + 1]
 
     @pytest.mark.parametrize('b', numbers)
     def test_add(self, b):
@@ -141,38 +141,38 @@ class TestMath(object):
         with pytest.raises(ZeroDivisionError):
             divmod(1, mpz(0))
 
-    @pytest.mark.parametrize('b', {0, 2, 1 << 16})
+    @pytest.mark.parametrize('b', [0, 2, 1 << 16])
     def test_shifts(self, b):
         assert mpz(1) << mpz(b) == mpz(1 << b)
         assert mpz(1) << b == mpz(1 << b)
         assert mpz(1 << 100) >> mpz(b) == mpz((1 << 100) >> b)
         assert mpz(1 << 100) >> b == mpz((1 << 100) >> b)
 
-    @pytest.mark.parametrize('b', {0, 2, sys.maxsize, MAX_UI})
+    @pytest.mark.parametrize('b', [0, 2, sys.maxsize, MAX_UI])
     def test_rshifts(self, b):
         assert b << mpz(1) == mpz(b << 1)
         assert b >> mpz(1) == mpz(b >> 1)
 
-    @pytest.mark.parametrize('b', {-1, MAX_UI + 1})
+    @pytest.mark.parametrize('b', [-1, MAX_UI + 1])
     def test_shifts_invalid_shift(self, b):
         with pytest.raises(OverflowError):
             mpz(1) << b
         with pytest.raises(OverflowError):
             mpz(1) >> b
 
-    @pytest.mark.parametrize('type_', {int, long, mpz})
+    @pytest.mark.parametrize('type_', [int, long, mpz])
     def test_shifts_valid_type(self, type_):
         assert mpz(1) << type_(1) == mpz(2)
         assert mpz(4) >> type_(1) == mpz(2)
 
-    @pytest.mark.parametrize('type_', {float, str})
+    @pytest.mark.parametrize('type_', [float, str])
     def test_shifts_invalid_type(self, type_):
         with pytest.raises(TypeError):
             mpz(1) << type_(1)
         with pytest.raises(TypeError):
             mpz(1) >> type_(1)
 
-    @pytest.mark.parametrize('type_', {float, str})
+    @pytest.mark.parametrize('type_', [float, str])
     def test_rshifts_invalid_type(self, type_):
         with pytest.raises(TypeError):
             type_(1) << mpz(1)
@@ -193,7 +193,7 @@ class TestMath(object):
 
     def test_conversions_int(self):
         for n in self.numbers:
-            for type_ in {int, long}:
+            for type_ in [int, long]:
                 n1 = type_(n)
                 mpz_n = type_(mpz(n))
                 assert type(n1) == type(mpz_n)
@@ -247,15 +247,15 @@ class TestMath(object):
         assert not mpz(0)
         assert mpz(-1)
 
-    @pytest.mark.parametrize('b', {-1, 0, 1, 1024, MAX_UI + 1})
+    @pytest.mark.parametrize('b', [-1, 0, 1, 1024, MAX_UI + 1])
     def test_pow_no_mod(self, b):
         if b < 0:
-            for exp in {mpz(b), b}:
+            for exp in [mpz(b), b]:
                 with pytest.raises(ValueError) as exc:
                     mpz(2) ** exp
                 assert exc.value.args == ('mpz.pow with negative exponent',)
         elif b > MAX_UI:
-            for exp in {mpz(b), b}:
+            for exp in [mpz(b), b]:
                 with pytest.raises(ValueError) as exc:
                     mpz(2) ** exp
                 assert exc.value.args == ('mpz.pow with outragous exponent',)
@@ -264,11 +264,11 @@ class TestMath(object):
             assert mpz(2) ** mpz(b) == res
             assert mpz(2) ** b == res
 
-    @pytest.mark.parametrize('b', {-1, 0, 1, 1024, MAX_UI + 1})
+    @pytest.mark.parametrize('b', [-1, 0, 1, 1024, MAX_UI + 1])
     def test_pow_with_mod(self, b):
         if b < 0:
-            for exp in {mpz(b), b}:
-                for mod in {mpz(7), 7}:
+            for exp in [mpz(b), b]:
+                for mod in [mpz(7), 7]:
                     with pytest.raises(ValueError) as exc:
                         pow(mpz(2), exp, mod)
                     assert exc.value.args == ('mpz.pow with negative exponent',)
