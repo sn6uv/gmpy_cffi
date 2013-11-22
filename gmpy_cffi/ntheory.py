a8a9848de01b360c5edad3a67833e29e1266c4e7
Angus Griffith <16sn6uv@gmail.com>
2013-11-23 01:36:59 +1100

    cleanup ntheory typechecking

--- gmpy_cffi/ntheory.py
+++ gmpy_cffi/ntheory.py
@@ -12,6 +12,16 @@ if PY3:
     xrange = range
 
 
+def _check_mpz(function_name, value_name, value):
+    if isinstance(value, mpz):
+        return value
+    if isinstance(value, (int, long)):
+        return mpz(value)
+    else:
+        raise TypeError('%s() expected integer %s got %s' % (
+            function_name, value_name, type(value)))
+
+
 def is_prime(x, n=25):
     """
     is_prime(x[, n=25]) -> bool
@@ -20,20 +30,11 @@ def is_prime(x, n=25):
     definately composite. x is checked for small divisors and up
     to n Miller-Rabin tests are performed.
     """
-
-    if isinstance(x, mpz):
-        pass
-    elif isinstance(x, (int, long)):
-        x = mpz(x)
-    else:
-        raise TypeError('is_prime() expected integer x got %s' % type(x))
-
+    x = _check_mpz('is_prime', 'x', x)
     if not (isinstance(n, int) and n <= sys.maxsize):
         raise TypeError('is_prime() expected integer n got %s' % type(n))
-
     if n <= 0:
         raise ValueError("is_prime repitition count must be positive")
-
     return gmp.mpz_probab_prime_p(x._mpz, n) != 0
 
 
@@ -43,14 +44,7 @@ def next_prime(x):
 
     Return the next _probable_ prime number > x.
     """
-
-    if isinstance(x, mpz):
-        pass
-    elif isinstance(x, (int, long)):
-        x = mpz(x)
-    else:
-        raise TypeError('next_prime() expected integer x got %s' % type(x))
-
+    x = _check_mpz('next_prime', 'x', x)
     res = _new_mpz()
     gmp.mpz_nextprime(res, x._mpz)
     return mpz._from_c_mpz(res)
@@ -62,21 +56,8 @@ def gcd(a, b):
 
     Return the greatest common denominator of integers a and b.
     """
-
-    if isinstance(a, mpz):
-        pass
-    elif isinstance(a, (int, long)):
-        a = mpz(a)
-    else:
-        raise TypeError('gcd() expected integer a got %s' % type(a))
-
-    if isinstance(b, mpz):
-        pass
-    elif isinstance(b, (int, long)):
-        b = mpz(b)
-    else:
-        raise TypeError('gcd() expected integer b got %s' % type(b))
-
+    a = _check_mpz('gcd', 'a', a)
+    b = _check_mpz('gcd', 'b', b)
     res = _new_mpz()
     gmp.mpz_gcd(res, a._mpz, b._mpz)
     return mpz._from_c_mpz(res)
@@ -89,20 +70,8 @@ def gcdext(a, b):
     Return a 3-element tuple (g,s,t) such that
         g == gcd(a,b) and g == a*s + b*t
     """
-    if isinstance(a, mpz):
-        pass
-    elif isinstance(a, (int, long)):
-        a = mpz(a)
-    else:
-        raise TypeError('gcdex() expected integer a got %s' % type(a))
-
-    if isinstance(b, mpz):
-        pass
-    elif isinstance(b, (int, long)):
-        b = mpz(b)
-    else:
-        raise TypeError('gcdex() expected integer b got %s' % type(b))
-
+    a = _check_mpz('gcdext', 'a', a)
+    b = _check_mpz('gcdext', 'b', b)
     mpz_g, mpz_s, mpz_t = _new_mpz(), _new_mpz(), _new_mpz()
     gmp.mpz_gcdext(mpz_g, mpz_s, mpz_t, a._mpz, b._mpz)
     return (mpz._from_c_mpz(mpz_g), mpz._from_c_mpz(mpz_s), mpz._from_c_mpz(mpz_t))
@@ -114,21 +83,8 @@ def lcm(a, b):
 
     Return the lowest common multiple of integers a and b.
     """
-
-    if isinstance(a, mpz):
-        pass
-    elif isinstance(a, (int, long)):
-        a = mpz(a)
-    else:
-        raise TypeError('lcm() expected integer a got %s' % type(a))
-
-    if isinstance(b, mpz):
-        pass
-    elif isinstance(b, (int, long)):
-        b = mpz(b)
-    else:
-        raise TypeError('lcm() expected integer b got %s' % type(b))
-
+    a = _check_mpz('lcm', 'a', a)
+    b = _check_mpz('lcm', 'b', b)
     res = _new_mpz()
     gmp.mpz_lcm(res, a._mpz, b._mpz)
     return mpz._from_c_mpz(res)
@@ -141,20 +97,8 @@ def invert(x, m):
     Return y such that x*y == 1 (mod m). Raises ZeroDivisionError if no
     inverse exists.
     """
-    if isinstance(x, mpz):
-        pass
-    elif isinstance(x, (int, long)):
-        x = mpz(x)
-    else:
-        raise TypeError('invert() expected integer x got %s' % type(x))
-
-    if isinstance(m, mpz):
-        pass
-    elif isinstance(m, (int, long)):
-        m = mpz(m)
-    else:
-        raise TypeError('lcm() expected integer m got %s' % type(m))
-
+    x = _check_mpz('invert', 'x', x)
+    m = _check_mpz('invert', 'm', m)
     res = _new_mpz()
     if gmp.mpz_invert(res, x._mpz, m._mpz) == 0:
         raise ZeroDivisionError
