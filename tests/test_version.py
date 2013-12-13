9d18860620fba1ca4e4143c75c9bb2dbab4a8bd5
Angus Griffith <16sn6uv@gmail.com>
2013-12-13 17:20:20 +1100

    add test_version

---
+++ tests/test_version.py
@@ -0,0 +1,25 @@
+import re
+
+from gmpy_cffi import version, mp_version, mpfr_version, mpc_version
+
+
+class TestVersion:
+    def test_version(self):
+        s = version()
+        assert isinstance(s, str)
+        assert re.match(r'\d\.\d\.\d', s)
+
+    def test_mp_version(self):
+        s = mp_version()
+        assert isinstance(s, str)
+        assert s.startswith('GMP ')
+
+    def test_mpfr_version(self):
+        s = mpfr_version()
+        assert isinstance(s, str)
+        assert s.startswith('MPFR ')
+
+    def test_mpc_version(self):
+        s = mpc_version()
+        assert isinstance(s, str)
+        assert s.startswith('MPC ')
